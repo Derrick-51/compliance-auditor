70d668f6f42434fad7daa3d1fe685f33ac8f2d3b
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-02-02 02:21:38 -0500

    Fix typos

--- nest_backend/src/photos/photos.controller.ts
+++ nest_backend/src/photos/photos.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common
 import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('')
-export class ProductsController {
+export class PhotosController {
 
     // Files will be named with dealer id with sequential numbering
     @Post('dealer/:id/upload-photos')
     @UseInterceptors(FilesInterceptor('photo'))
     uploadPhotos(@UploadedFiles() photos: Array<Express.Multer.File>): any {
-        console.log('upload photo')
+        console.log('upload photos')
     }
 }
