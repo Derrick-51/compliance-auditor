ebe472bac2dcb2d7e8b4248481b4fc4cf96747f4
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-02-02 04:45:34 -0500

    Implement photo upload controller

--- nest_backend/src/photos/photos.controller.ts
+++ nest_backend/src/photos/photos.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path'
 
 @Controller('')
 export class PhotoController {
 
     // Files will be named with dealer id with sequential numbering
     @Post('dealer/:id/upload-photo')
-    @UseInterceptors(FileInterceptor('photo'))
-    uploadPhotos(@UploadedFile() photo: Express.Multer.File): any {
-        console.log('upload photo')
+    @UseInterceptors(FileInterceptor('photo', {
+        storage: diskStorage({
+            destination: './uploaded-photos',
+            filename: (req, file, callback) => {
+                const uniqueName = 'placeholder'
+                const extension = extname(file.originalname)
+                const filename = `${uniqueName}${extension}`
+
+                callback(null, filename)
+            }
+        })
+    }))
+    uploadPhoto(@UploadedFile() photo: Express.Multer.File) {
+        console.log('photo upload', photo)
     }
 }
