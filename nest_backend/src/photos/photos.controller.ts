d8c413c626bddb6ce55917f1c8d6f5f66539aef9
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-02-02 03:52:29 -0500

    Change to single file upload

--- nest_backend/src/photos/photos.controller.ts
+++ nest_backend/src/photos/photos.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('')
-export class PhotosController {
+export class PhotoController {
 
     // Files will be named with dealer id with sequential numbering
-    @Post('dealer/:id/upload-photos')
-    @UseInterceptors(FilesInterceptor('photo'))
-    uploadPhotos(@UploadedFiles() photos: Array<Express.Multer.File>): any {
-        console.log('upload photos')
+    @Post('dealer/:id/upload-photo')
+    @UseInterceptors(FileInterceptor('photo'))
+    uploadPhotos(@UploadedFile() photo: Express.Multer.File): any {
+        console.log('upload photo')
     }
 }
