7a68dd341c0025b5f5e3d0376fc84ca2d2924b6c
Zwar Ahmed <zwarahmed01@gmail.com>
2024-02-02 18:11:45 -0500

    made a base component for the login page

---
+++ angular_frontend/src/app/loginpage/loginpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-loginpage',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './loginpage.component.html',
+  styleUrl: './loginpage.component.css'
+})
+export class LoginpageComponent {
+
+}
