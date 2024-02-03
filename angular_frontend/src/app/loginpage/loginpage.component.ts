1ba5033456074d5598f1e91daf3c9a4dcc3dd9e0
Zwar Ahmed <zwarahmed01@gmail.com>
2024-02-03 18:44:50 -0500

    created loginForm inside the LoginpageComponent to start linking the form data with Typescript.

--- angular_frontend/src/app/loginpage/loginpage.component.ts
+++ angular_frontend/src/app/loginpage/loginpage.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, EmailValidator} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, EmailValidator, FormGroup} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import { EmailfieldComponent } from '../emailfield/emailfield.component';
 import { PasswordfieldComponent } from '../passwordfield/passwordfield.component';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-loginpage',
@@ -17,10 +19,14 @@ import { PasswordfieldComponent } from '../passwordfield/passwordfield.component
     ReactiveFormsModule,
     EmailfieldComponent,
     PasswordfieldComponent,
+    MatButtonModule,
   ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
-
-}
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+  }
