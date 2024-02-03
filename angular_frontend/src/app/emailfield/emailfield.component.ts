c3c7f403248f9c0e0c07564d563d49a0917ec56d
Zwar Ahmed <zwarahmed01@gmail.com>
2024-02-02 22:09:00 -0500

    seperated the email and login card components

---
+++ angular_frontend/src/app/emailfield/emailfield.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emailfield',
+  standalone: true,
+  imports: [],
+  templateUrl: './emailfield.component.html',
+  styleUrl: './emailfield.component.css'
+})
+export class EmailfieldComponent {
+
+}
--- angular_frontend/src/app/loginpage/loginpage.component.ts
+++ angular_frontend/src/app/loginpage/loginpage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, EmailValidator} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import { EmailfieldComponent } from '../emailfield/emailfield.component';
 
 @Component({
   selector: 'app-loginpage',
@@ -13,6 +14,7 @@ import {MatInputModule} from '@angular/material/input';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
+    EmailfieldComponent,
   ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
