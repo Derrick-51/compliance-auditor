a24134012d5fbc5cf5e05a85467fc816e3addb13
Zwar Ahmed <zwarahmed01@gmail.com>
2024-02-02 19:20:23 -0500

    added email field to the login screen

--- angular_frontend/src/app/loginpage/loginpage.component.ts
+++ angular_frontend/src/app/loginpage/loginpage.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
