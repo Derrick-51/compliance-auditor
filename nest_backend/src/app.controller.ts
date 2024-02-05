8a07bf32b2e80bac31865adc9060448e61162d0d
Rey Jakosalem <reyjakosa@gmail.com>
2024-02-05 15:35:03 -0500

    Modified app.controller and app.service to create a registration feature
    only the login is needed for now for prototype 1 but I'm getting a head start on the other features while waiting for the frontend to be completed

--- nest_backend/src/app.controller.ts
+++ nest_backend/src/app.controller.ts
@@ -1,5 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as bcrypt from 'bcrypt';
 
-@Controller()
-export class AppController {}
+@Controller('api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('register')
+  async register(
+    @Body('email') email: string,
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    return this.appService.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+  }
+}
--- nest_backend/src/app.service.ts
+++ nest_backend/src/app.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(data: any): Promise<User> {
+    return this.userRepository.save(data);
+  }
+}
