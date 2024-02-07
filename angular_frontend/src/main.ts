b6349e39ca36a793fa9e5a346f7195f97f5a13c1
Zwar Ahmed <zwarahmed01@gmail.com>
2024-02-07 18:05:19 -0500

    fixed merge conflict from main.ts, removed routing module

--- angular_frontend/src/main.ts
+++ angular_frontend/src/main.ts
@@ -1,13 +1,12 @@
 import { importProvidersFrom, mergeApplicationConfig } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
+        importProvidersFrom(BrowserModule),
         provideClientHydration(),
         provideAnimationsAsync()
     ]
