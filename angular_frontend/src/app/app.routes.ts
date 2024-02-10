0965a39894b77fd382ba40b70a8e3d24f00f09d8
ZwarA01 <78764126+ZwarA01@users.noreply.github.com>
2024-02-10 15:10:01 -0500

    added material UI

--- angular_frontend/src/app/app.routes.ts
+++ angular_frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full', //this will be a 404 page, when the user tries to go to a page that doesnt exist
+  },
+];
