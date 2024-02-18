89fca1816b53488fcae20cb7251bd59e14a9880c
Shane OLoughlin <ft4340@wayne.edu>
2024-02-18 14:27:28 -0500

    Make an audit page with multiple image upload and previews

--- angular_frontend/src/app/app.routes.ts
+++ angular_frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ImageUploadComponent } from './components/image-upload/image-upload.component';
+import { ReviewAuditComponent } from './components/review-audit/review-audit.component';
+import { StatusComponent } from './components/status/status.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', component: HomeComponent},
+{path: 'status', component: StatusComponent},
+{path: 'image-upload', component: ImageUploadComponent},
+{path: 'override', component: ReviewAuditComponent}
+];
---
+++ angular_frontend/src/app/components/image-upload/image-upload.component.ts
@@ -0,0 +1,82 @@
+import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpResponse } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FileUploadService } from '../../services/file-upload.service';
+
+@Component({
+  selector: 'app-image-upload',
+  standalone: true,
+  imports: [CommonModule, RouterLink, MatButtonModule, MatCardModule],
+  templateUrl: './image-upload.component.html',
+  styleUrl: './image-upload.component.scss',
+})
+export class ImageUploadComponent {
+  currentFiles?: FileList;
+  messages: string[] = [];
+  previews: string[] = [];
+
+  // Injects file upload service
+  constructor(private uploadService: FileUploadService) {}
+
+  // Updates preview
+  selectFiles(event: any): void {
+    this.messages = [];
+    this.previews = [];
+    this.currentFiles = event.target.files;
+
+    if (this.currentFiles && this.currentFiles[0]) {
+      const numberOfFiles = this.currentFiles.length;
+      for (let i = 0; i < numberOfFiles; i++) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          console.log(e.target.result);
+          this.previews.push(e.target.result);
+        };
+        reader.readAsDataURL(this.currentFiles[i]);
+      }
+    }
+  }
+
+  // Upload for each individual image
+  upload(file:File): void {
+    if (file) {
+      this.uploadService.upload(file).subscribe({
+      next: (event: any) => {
+        if (event instanceof HttpResponse) {
+          const msg = file.name + ': Successful!';
+          this.messages.push(msg);
+        }
+      },
+      error: (err: any) => {
+        let msg = file.name + ': Failed!';
+        if (err.error && err.error.message) {
+          msg += ' ' + err.error.message;
+        }
+        this.messages.push(msg);
+      },
+        complete: () => {
+          this.currentFiles = undefined;
+        },
+      });
+    }
+  }
+
+  // Uploads all images
+  uploadFiles(): void {
+    this.messages = [];
+  
+    if (this.currentFiles) {
+      for (let i = 0; i < this.currentFiles.length; i++) {
+        this.upload(this.currentFiles[i]);
+      }
+    }
+  }
+
+/*  removeSelectedFile(index) {
+    delete file from FileList
+    this.currentFiles.splice(index, 1);
+   } */
+} 
