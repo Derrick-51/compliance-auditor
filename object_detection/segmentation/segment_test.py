c49edc67d2cd7c85b75c2f2d933b2fcf7769ab00
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-02-19 00:46:02 -0500

    Draw fastSAM masks

--- object_detection/segmentation/segment_test.py
+++ object_detection/segmentation/segment_test.py
@@ -1,29 +1,31 @@
-from ultralytics.models.sam import Predictor as SAMPredictor
-import torch
+from ultralytics import FastSAM
+from ultralytics.models.fastsam import FastSAMPrompt
+import numpy as np
 import cv2
 
-overrides = dict(conf=0.25, task="segment", mode="predict", imgsz=1024, model="sam_l.pt")
-predictor = SAMPredictor(overrides=overrides)
+source = "img3.jpg"
 
-# Set image to encode once
-image = cv2.imread("img1.jpg")
-image = torch.tensor(image)
-imgHeight, imgLength, _ = image.shape
-bbox = [(imgLength/4), (imgHeight/4), ((imgLength/4)*3), ((imgLength/4)*3)]
-# predictor.set_image(image)
+model = FastSAM("FastSAM-s.pt")
 
+allResults = model(source, device="cpu", retina_masks=True, imgsz=1024, conf=0.4, iou=0.9)
 
-results = predictor.inference(
-    image,
-    bboxes=[bbox],
-    points=None,
-    labels=None,
-    masks=None,
-    multimask_output=True)
+promptProcess = FastSAMPrompt(source, allResults, device="cpu")
 
-print(results)
+results = promptProcess.everything_prompt()
 
-# predictor.reset_image()
+img = np.copy(results[0].orig_img)
+bMask = np.zeros(img.shape[:2], np.uint8)
 
-cv2.imshow("test", image)
+for count, result in enumerate(results):
+    contour = result.masks.xy.pop()
+    contour = contour.astype(np.int32)
+    contour = contour.reshape(-1, 1, 2)
+
+    cv2.drawContours(bMask, [contour], -1, (255, 255, 255), cv2.FILLED)
+    cv2.imshow(f'Contour: {count}', bMask)
+    
 cv2.waitKey(0)
+
+
+
+
