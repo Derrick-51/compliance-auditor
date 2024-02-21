7a5d4a3b6a600d65f616eec4e3e6210f0e8e6e80
ZwarA01 <78764126+ZwarA01@users.noreply.github.com>
2024-02-21 15:28:30 -0500

    removed homepage and navbar from login/register page as requested by client

--- angular_frontend/src/app/app.routes.ts
+++ angular_frontend/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }, //this will be a 404 page, when the user tries to go to a page that doesnt exist }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }, //this will be a 404 page, when the user tries to go to a page that doesnt exist }
 ];
