6f2f48ce3575510276fc055a01f45536ba7c5cd1
Rey Jakosalem <reyjakosa@gmail.com>
2024-02-28 16:20:10 -0500

    commit before pulling

--- angular_frontend/src/app/components/review-audit/review-audit.component.ts
+++ angular_frontend/src/app/components/review-audit/review-audit.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { navbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-review-audit',
   standalone: true,
-  imports: [],
+  imports: [navbarComponent],
   templateUrl: './review-audit.component.html',
-  styleUrl: './review-audit.component.scss'
+  styleUrl: './review-audit.component.scss',
 })
-export class ReviewAuditComponent {
-
-}
+export class ReviewAuditComponent {}
