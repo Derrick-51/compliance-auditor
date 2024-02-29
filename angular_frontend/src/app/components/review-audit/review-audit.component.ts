32dc36af0eb63ff6a939307a57d0c08a0cc55d8c
Rey Jakosalem <reyjakosa@gmail.com>
2024-02-29 09:50:09 -0500

    Importing HTTP for listing audits in Audit Review (incomplete)

--- angular_frontend/src/app/components/review-audit/review-audit.component.ts
+++ angular_frontend/src/app/components/review-audit/review-audit.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { navbarComponent } from '../navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
+import { MessageBundle } from '@angular/compiler';
 
 @Component({
   selector: 'app-review-audit',
   standalone: true,
-  imports: [navbarComponent],
+  imports: [navbarComponent, HttpClient],
   templateUrl: './review-audit.component.html',
   styleUrl: './review-audit.component.scss',
 })
-export class ReviewAuditComponent {}
+export class ReviewAuditComponent {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageBundle
+  ) {}
+}
