b1d697cd03b9fdd572167d8b35bf009dcf9836d2
Rey Jakosalem <reyjakosa@gmail.com>
2024-03-01 11:58:23 -0500

    Editing audit table and grabbing a list of all audits

--- angular_frontend/src/app/components/review-audit/review-audit.component.ts
+++ angular_frontend/src/app/components/review-audit/review-audit.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { navbarComponent } from '../navbar/navbar.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MessageService } from '../../messages/message.service';
 
 @Component({
   selector: 'app-review-audit',
   standalone: true,
-  imports: [],
+  imports: [navbarComponent],
   templateUrl: './review-audit.component.html',
-  styleUrl: './review-audit.component.scss'
+  styleUrl: './review-audit.component.scss',
 })
 export class ReviewAuditComponent {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
+  private log(message: string) {
+    this.messageService.add('AuditService: ${message}');
+  }
+
+  private auditsUrl = 'api/audit';
 }
