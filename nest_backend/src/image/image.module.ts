0079858aa0bbddd5ab8816960c56e3938a47a0d6
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-03-04 19:42:20 -0500

    Add image repository

--- nest_backend/src/image/image.module.ts
+++ nest_backend/src/image/image.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Images } from './entities/image.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Images])],
   controllers: [ImageController],
   providers: [ImageService],
 })
--- nest_backend/src/image/image.service.ts
+++ nest_backend/src/image/image.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import { Images } from './entities/image.entity'
 
 @Injectable()
 export class ImageService {
+  constructor(
+    @InjectRepository(Images)
+    private imageRepository: Repository<Images>,
+  ) {}
+
   create(createImageDto: CreateImageDto) {
     return 'This action adds a new image';
   }
