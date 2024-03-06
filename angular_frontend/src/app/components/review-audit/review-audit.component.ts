6cd8ac68bd9bff83cfe9e876416b616bcb691840
ZwarA01 <78764126+ZwarA01@users.noreply.github.com>
2024-03-06 17:43:06 -0500

    added audit navbar to edit guidelines and review audit page

--- angular_frontend/src/app/components/review-audit/review-audit.component.ts
+++ angular_frontend/src/app/components/review-audit/review-audit.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AuditorNavbarComponent } from '../auditor-navbar/auditor-navbar.component';
 
 @Component({
   selector: 'app-review-audit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AuditorNavbarComponent],
   templateUrl: './review-audit.component.html',
   styleUrl: './review-audit.component.scss'
 })
