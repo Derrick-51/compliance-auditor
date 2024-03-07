3aa29ca7840765cbeecc6b7d63b045b0641ec729
Rey Jakosalem <105830872+Rey-Jey@users.noreply.github.com>
2024-03-07 12:06:38 -0500

    Database tweaking, postgres --> mssql

--- nest_backend/src/audit/entities/audit.entity.ts
+++ nest_backend/src/audit/entities/audit.entity.ts
@@ -23,7 +23,7 @@ export class Audit {
   @Column()
   dueDate: Date;
 
-  @Column({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" }) //for mssql { type: 'datetime' }
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' }) //for mssql { type: 'datetime' } //for postgres{ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" }
   update: Date;
 
   @ManyToOne((type) => Users, (user) => user.audit)
