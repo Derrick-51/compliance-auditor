e159c5f41aac5fe819f3fcdcba3c25eacb15294b
ZwarA01 <78764126+ZwarA01@users.noreply.github.com>
2024-03-19 16:09:03 -0400

    created controller and service for password reset

--- nest_backend/src/password/password.controller.ts
+++ nest_backend/src/password/password.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PasswordService } from './password.service';
 
 @Controller('password')
-export class PasswordController {}
+export class PasswordController {
+
+  constructor(private passwordService: PasswordService){
+
+  }
+
+  @Post('forgot')
+  async forget(
+    @Body('email') email: string
+  ){
+    const token = Math.random().toString(20).substr(2,12); //probably want to change how this is created
+
+    await this.passwordService.create({
+      email,
+      token
+    });
+  }
+}
--- nest_backend/src/password/password.service.ts
+++ nest_backend/src/password/password.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PasswordEntity } from './password.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PasswordService {}
+export class PasswordService {
+
+  constructor(
+    @InjectRepository(PasswordEntity) private readonly passwordRepository: Repository<PasswordEntity>
+  ){
+
+  }
+
+  async create(body: any){
+    return this.passwordRepository.save(body);
+  }
+}
