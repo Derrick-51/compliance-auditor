2d7c5c295e8362c2d81808c49c9acca71ea9be11
ZwarA01 <78764126+ZwarA01@users.noreply.github.com>
2024-03-19 13:50:03 -0400

    created controller and service for reset password feature

--- nest_backend/src/password/password.module.ts
+++ nest_backend/src/password/password.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PasswordController } from './password.controller';
+import { PasswordService } from './password.service';
 
-@Module({})
+@Module({
+  controllers: [PasswordController],
+  providers: [PasswordService]
+})
 export class PasswordModule {}
