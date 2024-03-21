4b69c3bc9814082fa7f0e375f817439071507a27
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-03-20 20:42:18 -0400

    Implement finding one campaign with criteria attached

--- nest_backend/src/campaign/campaign.service.ts
+++ nest_backend/src/campaign/campaign.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
 import { UpdateCampaignDto } from './dto/update-campaign.dto';
+import { ReplOptions } from 'repl';
+import { Campaign } from './entities/campaign.entity';
 
 @Injectable()
 export class CampaignService {
+  constructor(
+    @InjectRepository(Campaign)
+    private readonly campaignRepository: Repository<Campaign>
+  ) {}
+
   create(createCampaignDto: CreateCampaignDto) {
     return 'This action adds a new campaign';
   }
@@ -14,6 +23,13 @@ export class CampaignService {
 
   findOne(id: number) {
     return `This action returns a #${id} campaign`;
+  }
+
+  async findOneWithCriteria(id: number) {
+    return await this.campaignRepository.findOne({
+      where: {campaignID: id},
+      relations: {criteria: true}
+    });
   }
 
   update(id: number, updateCampaignDto: UpdateCampaignDto) {
