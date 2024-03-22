591df6e2f2782d7080b575ad8d56055cd1448c3a
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-03-22 01:57:59 -0400

    Make campaign list skeleton

--- angular_frontend/src/app/components/campaign-list/campaign-list.component.ts
+++ angular_frontend/src/app/components/campaign-list/campaign-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AuditorNavbarComponent } from '../auditor-navbar/auditor-navbar.component';
 
 @Component({
   selector: 'app-campaign-list',
   standalone: true,
-  imports: [],
+  imports: [AuditorNavbarComponent],
   templateUrl: './campaign-list.component.html',
   styleUrl: './campaign-list.component.scss'
 })
