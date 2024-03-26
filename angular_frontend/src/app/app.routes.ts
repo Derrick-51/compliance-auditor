10018495e48d667c299e7088063227018c8cc418
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-03-26 03:57:53 -0400

    Change campaign-list route to campaigns

--- angular_frontend/src/app/app.routes.ts
+++ angular_frontend/src/app/app.routes.ts
@@ -24,7 +24,7 @@ export const routes: Routes = [
   { path: 'make-an-audit', component: MakeAnAuditComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: 'audit-navbar', component: AuditorNavbarComponent },
-  { path: 'campaign-list', component: CampaignListComponent },
+  { path: 'campaigns', component: CampaignListComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }, //this will be a 404 page, when the user tries to go to a page that doesnt exist }
 ];
