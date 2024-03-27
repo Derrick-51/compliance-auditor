6ed6bd4904e2d2b44784e504a1e812542ff188cd
Derrick-51 <95022335+Derrick-51@users.noreply.github.com>
2024-03-27 02:25:46 -0400

    Update image api path

--- nest_backend/src/image/image.controller.ts
+++ nest_backend/src/image/image.controller.ts
@@ -9,7 +9,7 @@ import {v4 as uuidv4} from "uuid";
 import { CriteriaService } from 'src/criteria/criteria.service';
 import { AuditService } from 'src/audit/audit.service';
 
-@Controller('image')
+@Controller('api/image')
 export class ImageController {
   constructor(
     private readonly imageService: ImageService,
